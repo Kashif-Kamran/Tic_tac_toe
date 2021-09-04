84285f4a8e9c827b5f70075017b0c3d211bdae02
Kashif-Kamran <i190541@nu.edu.pk>
2021-09-04 20:35:49 +0500

    Filling of grid according to the numpad is working fine

--- arina.h
+++ arina.h
@@ -21,11 +21,11 @@ public:
     void display()
     {
         cout << "_____________" << endl;
-        cout << "| " << array[0][0] << " | " << array[0][1] << " | " << array[0][2] << " |" << endl;
+        cout << "| " << array[2][0] << " | " << array[2][1] << " | " << array[2][2] << " |" << endl;
         cout << "-------------" << endl;
         cout << "| " << array[1][0] << " | " << array[1][1] << " | " << array[1][2] << " |" << endl;
         cout << "-------------" << endl;
-        cout << "| " << array[2][0] << " | " << array[2][1] << " | " << array[2][2] << " |" << endl;
+        cout << "| " << array[0][0] << " | " << array[0][1] << " | " << array[0][2] << " |" << endl;
         cout << "-------------" << endl;
     }
     void writeArina(int row, int col, char deli)
@@ -39,5 +39,25 @@ public:
             {
                 array[i][j] = ' ';
             }
+    }
+    bool checkWin()
+    {
+        int hor_check_count = 0;
+        int ver_check_count = 0;
+        int dig_check_count = 0;
+        // Horizendtal Checking
+        for (int i = 0; i < 3; i++)
+        {
+            hor_check_count = 0;
+            for (int j = 1; j < 3; j++)
+            {
+                array[i][j - 1] = array[i][j];
+                if (hor_check_count == 2)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
--- main.cpp
+++ main.cpp
@@ -9,16 +9,26 @@ int main()
     {
         cout << "Enter number from 1 to 8 : ";
         cin >> index;
-        while (index < 1 or index > 8)
+        while (index < 1 or index > 9)
         {
             cout << "Enter the number again : ";
             cin >> index;
         }
 
-        int row = index / 3;
-        int col = ((index + 1) % 3) - 1;
+        int row = (index - 1) / 3;
+        int col = -2;
+        if (index % 3 == 0)
+        {
+            col = 2;
+        }
+        else
+        {
+            col = ((index) % 3) - 1;
+        }
         cout << "Row : " << row << endl;
         cout << "Col : " << col << endl;
+        arina.writeArina(row, col, '0');
+        arina.display();
     }
     return 0;
 }
